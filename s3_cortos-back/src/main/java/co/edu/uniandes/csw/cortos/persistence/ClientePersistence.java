85db995f54aa3dcc69b0c0abe9506969f1e58e92
Arturo <a.rubio@uniandes.edu.co>
2019-08-22 19:00:11 -0500

    se agrega el CRUD del cliente

--- s3_cortos-back/src/main/java/co/edu/uniandes/csw/cortos/persistence/ClientePersistence.java
+++ s3_cortos-back/src/main/java/co/edu/uniandes/csw/cortos/persistence/ClientePersistence.java
@@ -6,9 +6,13 @@
 package co.edu.uniandes.csw.cortos.persistence;
 
 import co.edu.uniandes.csw.cortos.entities.ClienteEntity;
+import static java.util.Collections.list;
+import java.util.List;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import static org.eclipse.persistence.jpa.jpql.utility.CollectionTools.list;
 
 /**
  *
@@ -24,5 +28,27 @@ public class ClientePersistence
     {
         em.persist(cliente);
         return cliente;
+    }
+    
+    public List<ClienteEntity> findAll()
+    {
+        TypedQuery query = em.createQuery("select u from ClienteEntity u", ClienteEntity.class);
+        return query.getResultList();
+    }
+    
+    public ClienteEntity find (Long clienteId)
+    {
+        return em.find(ClienteEntity.class, clienteId);
+    }
+    
+    public ClienteEntity update(ClienteEntity clienteEntity)
+    {
+        return em.merge(clienteEntity);
+    }
+    
+    public void delete(Long clienteId)
+    {
+        ClienteEntity clienteEntity = em.find(ClienteEntity.class, clienteId);
+        em.remove(clienteEntity);
     }
 }
--- s3_cortos-back/src/test/java/co/edu/uniandes/csw/cortos/test/persistence/ClientePersistenceTest.java
+++ s3_cortos-back/src/test/java/co/edu/uniandes/csw/cortos/test/persistence/ClientePersistenceTest.java
@@ -7,14 +7,18 @@ package co.edu.uniandes.csw.cortos.test.persistence;
 
 import co.edu.uniandes.csw.cortos.entities.ClienteEntity;
 import co.edu.uniandes.csw.cortos.persistence.ClientePersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -29,17 +33,57 @@ public class ClientePersistenceTest
 {
     @PersistenceContext
     protected EntityManager em;
+    
+    @Inject
+    ClientePersistence cp;
+    
+    @Inject
+    UserTransaction utx;
+    
+    private List<ClienteEntity> data = new ArrayList<>();
+    
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            em.joinTransaction();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    private void clearData() {
+        em.createQuery("delete from ClienteEntity").executeUpdate();
+    }
+
+    private void insertData() {
+        PodamFactory factory = new PodamFactoryImpl();
+        for (int i = 0; i < 3; i++) {
+            ClienteEntity entity = factory.manufacturePojo(ClienteEntity.class);
+
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
    
     @Deployment
     public static JavaArchive createDeployment()
     {
-        return ShrinkWrap.create(JavaArchive.class).addClass(ClienteEntity.class)
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(ClienteEntity.class)
                 .addClass(ClientePersistence.class)
                 .addAsManifestResource("META-INF/persistence.xml","persistence.xml")
                 .addAsManifestResource("META-INF/beans.xml","beans.xml");
     }
-    @Inject
-    ClientePersistence cp;
+    
     
     @Test
     public void createTest()
@@ -52,5 +96,53 @@ public class ClientePersistenceTest
         
         Assert.assertEquals(cliente.getNombre(), entity.getNombre());
         
+    }
+    
+    @Test
+    public void getClientesTest() {
+        List<ClienteEntity> list = cp.findAll();
+        Assert.assertEquals(data.size(), list.size());
+        for (ClienteEntity ent : list) {
+            boolean found = false;
+            for (ClienteEntity entity : data) {
+                if (ent.getId().equals(entity.getId())) {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void getClienteTest() {
+        ClienteEntity entity = data.get(0);
+        ClienteEntity newEntity = cp.find(entity.getId());
+        Assert.assertNotNull(newEntity);
+        Assert.assertEquals(entity.getNombre(), newEntity.getNombre());
+        Assert.assertEquals(entity.getContrasenia(), entity.getContrasenia());
+        Assert.assertEquals(entity.getCorreo(), entity.getCorreo());    
+    }
+    
+    @Test
+    public void updateClienteTest() {
+        ClienteEntity entity = data.get(0);
+        PodamFactory factory = new PodamFactoryImpl();
+        ClienteEntity newEntity = factory.manufacturePojo(ClienteEntity.class);
+
+        newEntity.setId(entity.getId());
+
+        cp.update(newEntity);
+
+        ClienteEntity resp = em.find(ClienteEntity.class, entity.getId());
+
+        Assert.assertEquals(newEntity.getNombre(), resp.getNombre());
+    }
+    
+    @Test
+    public void deleteClienteTest() {
+        ClienteEntity entity = data.get(0);
+        cp.delete(entity.getId());
+        ClienteEntity deleted = em.find(ClienteEntity.class, entity.getId());
+        Assert.assertNull(deleted);
     }
 }
