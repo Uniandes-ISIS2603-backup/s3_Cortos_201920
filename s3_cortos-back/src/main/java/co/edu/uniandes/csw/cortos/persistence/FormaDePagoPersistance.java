405175d55a436305a0234c8c7972a71b644470ca
Juan Felipe Mejia Parra <jf.mejia12@uniandes.edu.co>
2019-08-28 20:15:13 -0500

    Se agregaron los metodos y test CRUD para FormaDePagoPersistence

---
+++ s3_cortos-back/src/main/java/co/edu/uniandes/csw/cortos/persistence/FormaDePagoPersistance.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.uniandes.csw.cortos.persistence;
+
+import co.edu.uniandes.csw.cortos.entities.FormaDePagoEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author Juan Felipe Mejia Parra
+ */
+@Stateless
+public class FormaDePagoPersistance {
+    
+    private static final Logger LOGGER  = Logger.getLogger(FormaDePagoPersistance.class.getName());
+    @PersistenceContext(unitName = "cortosPU")
+    protected EntityManager em;
+    
+    public FormaDePagoEntity create(FormaDePagoEntity formaDePago)
+    {
+        em.persist(formaDePago);
+        
+        return formaDePago;
+    }
+    
+    public List<FormaDePagoEntity> findAll()
+    {
+        TypedQuery query = em.createQuery("select u from FormaDePagoEntity u", FormaDePagoEntity.class);
+        return query.getResultList();
+    }
+    
+    public FormaDePagoEntity find (Long formaDePagoId)
+    {
+        return em.find(FormaDePagoEntity.class, formaDePagoId);
+    }
+    
+    public FormaDePagoEntity update(FormaDePagoEntity pFormaEntity)
+    {
+        return em.merge(pFormaEntity);
+    }
+    
+    public void delete(Long id)
+    {
+        FormaDePagoEntity borrar = em.find(FormaDePagoEntity.class, id);
+        em.remove(borrar);
+    }
+    
+}
