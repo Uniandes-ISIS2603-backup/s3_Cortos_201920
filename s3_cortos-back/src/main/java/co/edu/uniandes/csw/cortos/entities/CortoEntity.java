33f9038db11e4fd241f56e0af00d3b4d49ceb93a
pedro luis callejas <pl.callejas@uniandes.edu.co>
2019-09-25 13:36:19 -0500

    Se agregaron asociaciones en CortoEntity y CineastaEntity

--- s3_cortos-back/src/main/java/co/edu/uniandes/csw/cortos/entities/CortoEntity.java
+++ s3_cortos-back/src/main/java/co/edu/uniandes/csw/cortos/entities/CortoEntity.java
@@ -7,10 +7,12 @@ package co.edu.uniandes.csw.cortos.entities;
 
 import co.edu.uniandes.csw.cortos.podam.DateStrategy;
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import uk.co.jemos.podam.common.PodamExclude;
@@ -21,18 +23,36 @@ import uk.co.jemos.podam.common.PodamStrategyValue;
  * @author Juan Sebastian Gomez
  */
 @Entity
-public class CortoEntity extends BaseEntity implements Serializable{
+public class CortoEntity extends BaseEntity implements Serializable {
+
     @PodamExclude
     @OneToMany(mappedBy = "corto")
     private List<ComentarioEntity> comentarios;
-    
+
     @PodamExclude
-    @OneToMany(mappedBy="corto")
+    @OneToMany(mappedBy = "corto")
     private List<CalificacionEntity> calificaciones;
-    
+
     @PodamExclude
-    @OneToMany(mappedBy="corto")
+    @OneToMany(mappedBy = "corto")
     private List<TemaEntity> temas;
+
+    /**
+     * Asociación con la clase CineastaEntity,que describe el cineasta productor
+     * del corto.
+     */
+    @PodamExclude
+    @OneToOne
+    private CineastaEntity productor;
+
+    /**
+     * Asociación con la clase CineastaEntity,que describe los cineastas asociados 
+     * del corto.
+     */
+    @PodamExclude
+    @OneToMany(mappedBy = "cortoCineastas")
+    private List<CineastaEntity> cineasta = new ArrayList<CineastaEntity>();
+
     /**
      * nombre del corto
      */
@@ -41,7 +61,7 @@ public class CortoEntity extends BaseEntity implements Serializable{
      * Fecha de publicacion del corto
      */
     @Temporal(TemporalType.DATE)
-    @PodamStrategyValue(DateStrategy.class)        
+    @PodamStrategyValue(DateStrategy.class)
     Date fechaDePublicacion;
     /**
      * Descripcion del corto
@@ -55,67 +75,85 @@ public class CortoEntity extends BaseEntity implements Serializable{
      * Calificacion promedio del corto
      */
     Double calificacionPromedio;
+
     /**
      * @return nombre del corto
      */
     public String getNombre() {
         return nombre;
     }
+
     /**
      * Actualiza el nombre del corto, con el nombre recibido por parametro
-     * @param nombre 
+     *
+     * @param nombre
      */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     /**
      * @return la fecha de publicacion del corto
      */
     public Date getFechaDePublicacion() {
         return fechaDePublicacion;
     }
+
     /**
-     * Actualiza la fecha de publicacion del corto, con la fecha de publicacion recibida por parametro
-     * @param nombre 
+     * Actualiza la fecha de publicacion del corto, con la fecha de publicacion
+     * recibida por parametro
+     *
+     * @param nombre
      */
     public void setFechaDePublicacion(Date fechaDePublicacion) {
         this.fechaDePublicacion = fechaDePublicacion;
     }
+
     /**
-     * @return la descripcion del corto 
+     * @return la descripcion del corto
      */
     public String getDescripcion() {
         return descripcion;
     }
+
     /**
-     * Actualiza la descripcion del corto, con la descripcion recibida por parametro
-     * @param nombre 
+     * Actualiza la descripcion del corto, con la descripcion recibida por
+     * parametro
+     *
+     * @param nombre
      */
     public void setDescripcion(String descripcion) {
         this.descripcion = descripcion;
     }
+
     /**
      * @return el precio del corto
      */
     public Double getPrecio() {
         return precio;
     }
+
     /**
      * Actualiza el precio del corto, con el precio recibido por parametro
-     * @param nombre 
+     *
+     * @param nombre
      */
     public void setPrecio(Double precio) {
         this.precio = precio;
     }
+
     /**
-     * @return calificacion promedio del corto 
+     * @return calificacion promedio del corto
      */
     public Double getCalificacionPromedio() {
         return calificacionPromedio;
     }
+
     /**
-     * Actualiza la calificacion promedio del corto, con la calificacion recibida por parametro
-     * @param nombre 
+     * Actualiza la calificacion promedio del corto, con la calificacion
+     * recibida por parametro
+     *
+     * @param nombre
      */
     public void setCalificacionPromedio(Double calificacionPromedio) {
         this.calificacionPromedio = calificacionPromedio;
@@ -162,4 +200,34 @@ public class CortoEntity extends BaseEntity implements Serializable{
     public void setTemas(List<TemaEntity> temas) {
         this.temas = temas;
     }
+
+    /**
+     * @return the productor
+     */
+    public CineastaEntity getProductor() {
+        return productor;
+    }
+
+    /**
+     * @param productor the productor to set
+     */
+    public void setProductor(CineastaEntity productor) {
+        this.productor = productor;
+    }
+
+    /**
+     * @return the cineasta
+     */
+    public List<CineastaEntity> getCineasta() {
+        return cineasta;
+    }
+
+    /**
+     * @param cineasta the cineasta to set
+     */
+    public void setCineasta(List<CineastaEntity> cineasta) {
+        this.cineasta = cineasta;
+    }
+    
+    
 }
